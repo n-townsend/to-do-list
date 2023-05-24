932178a8f78023f55908b7821d2f5b127d90139d
n-townsend <132301374+n-townsend@users.noreply.github.com>
2023-05-24 12:45:26 -0400

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+function newItem(){
+
+    //Add new item to the list of items: 
+    
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+    
+    if (inputValue === '') {
+        alert("You must write something!");
+      } else {
+        $('#list').append(li);
+      }
+    
+     //2. Enable crossing out an item from the list of items:
+     function crossOut() {
+        li.toggleClass("strike");
+    }
+
+    li.on("dblclick", function crossOut() {
+        li.toggleClass("strike");
+    });
+     //Add a delete button: 
+     let crossOutButton = $('<crossOutButton></crossOutButton>');
+     crossOutButton.append(document.createTextNode('X'));
+     li.append(crossOutButton);
+
+     //CSS Link to delete button class 
+     crossOutButton.on("click", deleteListItem);
+     function deleteListItem(){
+           li.addClass("delete")
+       }
+     //Allows item sorting 
+    
+     $('#list').sortable();
+    
+    }
